60b9db33e41cd395fbb035cddf7e653d523446b6
Марк Андреев <andre3v.mark@yandex.ru>
2026-06-16 13:03:15 +0300

    scroll page fix

--- vite-project/src/router/index.ts
+++ vite-project/src/router/index.ts
@@ -29,5 +29,12 @@ const routes = [
 
 export const router = createRouter({
     history: createWebHistory(),
-    routes
+    routes,
+    scrollBehavior(_to, _from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition
+        }
+
+        return { top: 0, left: 0 }
+    }
 })
